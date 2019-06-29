9a94742a00072540ebe1c98bb8e27714489e1e0a
Allie Robinson <allierob93@gmail.com>
2019-06-28 18:15:54 -0700

    MVP met. App styled. All components and links are working properly.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,19 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+
+    const { name, age, height } = this.state
+    const newSmurf = { name, age, height };
+
+    axios.post("http://localhost:3333/smurfs", newSmurf)
+    .then((response) => {
+      this.setState({
+        smurfs: response.data
+      })
+    })
+    .catch((err) => {
+      console.log("err", err);
+    })
 
     this.setState({
       name: '',
@@ -31,23 +45,23 @@ class SmurfForm extends Component {
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
-            placeholder="name"
+            placeholder="Name"
             value={this.state.name}
             name="name"
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="age"
+            placeholder="Age"
             value={this.state.age}
             name="age"
           />
           <input
             onChange={this.handleInputChange}
-            placeholder="height"
+            placeholder="Height"
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button type="submit">Add to the Village</button>
         </form>
       </div>
     );
